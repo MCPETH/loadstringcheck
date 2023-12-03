514ee0793bf3452d58d5239352530f48c6a63774
MCPETH <thtongame@gmail.com>
2023-12-03 11:10:59 +0700

    Create loadstring.lua

---
+++ loadstring.lua
@@ -0,0 +1,9 @@
+if game.PlaceId == #paste id here then
+    loadstring(game:HttpGet("example.com"))()
+elseif game.PlaceId == #paste id here then
+    loadstring(game:HttpGet("example.com"))()
+elseif game.PlaceId == #paste id here then
+    loadstring(game:HttpGet("example.com"))()
+else 
+    game.Players.LocalPlayer:Kick("Games Not Supports")
+end
